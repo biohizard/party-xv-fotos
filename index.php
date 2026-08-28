b3c060a450fa665ebd7f1ba3bb2e0988567f25ff
XR8 - Hp Lap <biohizard@gmail.com>
2026-08-28 03:55:45 -0600

    update
    CROS

--- index.php
+++ index.php
@@ -87,6 +87,15 @@
 			color: red;
 		}
 	</style>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KMW67NJZ60"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-KMW67NJZ60');
+  </script>
 
 </head>
 
